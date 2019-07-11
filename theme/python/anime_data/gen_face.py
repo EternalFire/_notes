44e6907cc8d136e835a27840d7338fae4d732ce9
fire1206 <hzhfire@qq.com>
2019-07-11 19:20:13 +0700

    python

---
+++ theme/python/anime_data/gen_face.py
@@ -0,0 +1,37 @@
+import cv2
+import sys
+import os.path
+from glob import glob
+
+def detect(filename, cascade_file="lbpcascade_animeface.xml"):
+    if not os.path.isfile(cascade_file):
+        raise RuntimeError("%s: not found" % cascade_file)
+
+    cascade = cv2.CascadeClassifier(cascade_file)
+    image = cv2.imread(filename)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.equalizeHist(gray)
+
+    faces = cascade.detectMultiScale(gray,
+                                     # detector options
+                                     scaleFactor=1.1,
+                                     minNeighbors=5,
+                                     minSize=(48, 48))
+    for i, (x, y, w, h) in enumerate(faces):
+        face = image[y: y + h, x:x + w, :]
+        face = cv2.resize(face, (96, 96))
+        save_filename = '%s-%d.jpg' % (os.path.basename(filename).split('.')[0], i)
+        cv2.imwrite("faces/" + save_filename, face)
+
+
+if __name__ == '__main__':
+    if os.path.exists('faces') is False:
+        os.makedirs('faces')
+    file_list = glob('imgs/*.jpg')
+    for filename in file_list:
+        detect(filename)
+# --------------------- 
+# 作者：csdn_csdn__AI 
+# 来源：CSDN 
+# 原文：https://blog.csdn.net/heyc861221/article/details/80127914 
+# 版权声明：本文为博主原创文章，转载请附上博文链接！
