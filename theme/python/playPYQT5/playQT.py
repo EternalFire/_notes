8ca1654f96e7be9344e50daa53c1521b41ff3ea7
fire1206 <hzhfire@qq.com>
2019-08-07 20:30:24 +0700

    note

--- theme/python/playPYQT5/playQT.py
+++ theme/python/playPYQT5/playQT.py
@@ -3,9 +3,9 @@
 # http://zetcode.com/gui/pyqt5/
 #
 
-import sys, random
+import sys, random, os
 from PyQt5 import QtWidgets, QtCore, QtGui
-from PyQt5.QtCore import QDate, QTime, QDateTime, Qt, QTimeZone, pyqtSignal, QObject, QBasicTimer, QMimeData
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt, QTimeZone, pyqtSignal, QObject, QBasicTimer, QMimeData, QFile
 from PyQt5.QtGui import QIcon, QFont, QColor, QPixmap, QDrag, QPainter, QPen, QBrush, QPainterPath
 from PyQt5.QtWidgets import (QWidget, QToolTip,
     QPushButton, QApplication, QMessageBox, QDesktopWidget,
@@ -1032,6 +1032,121 @@ def demo_painter():
     sys.exit(app.exec_())
 
 
+def demo_custom_widget():
+    MAX_CAPACITY = 700
+    OVER_CAPACITY = 750
+
+    class Communicate(QObject):
+        updateBW = pyqtSignal(int)
+
+    class BurningWidget(QWidget):
+        def __init__(self):
+            super().__init__()
+            self.initUI()
+
+        def initUI(self):
+            self.setMinimumSize(1, 30)
+            self.value = 75
+            self.num = [75, 150, 225, 300, 375, 450, 525, 600, 675]
+
+        def setValue(self, value):
+            self.value = value
+
+        def paintEvent(self, e: QtGui.QPaintEvent) -> None:
+            qp = QPainter()
+            qp.begin(self)
+            self.drawWidget(qp)
+            qp.end()
+
+        def drawWidget(self, qp):
+
+            font = QFont("Serif", 7, QFont.Light)
+            qp.setFont(font)
+
+            size = self.size()
+            w = size.width()
+            h = size.height()
+
+            step = int(round(w / 10))
+
+            till = int(((w / OVER_CAPACITY) * self.value))
+            full = int(((w / OVER_CAPACITY) * MAX_CAPACITY))
+
+            if self.value >= MAX_CAPACITY:
+                qp.setPen(QColor(255, 255, 255))
+                qp.setBrush(QColor(255, 255, 184))
+                qp.drawRect(0, 0, full, h)
+
+                qp.setPen(QColor(255, 175, 175))
+                qp.setBrush(QColor(255, 175, 175))
+                qp.drawRect(full, 0, till - full, h)
+            else:
+                qp.setPen(QColor(255, 255, 255))
+                qp.setBrush(QColor(255, 255, 184))
+                qp.drawRect(0, 0, till, h)
+
+            pen = QPen(QColor(20, 20, 20), 1, Qt.SolidLine)
+            qp.setPen(pen)
+            qp.setBrush(Qt.NoBrush)
+            qp.drawRect(0, 0, w - 1, h - 1)
+
+            j = 0
+            for i in range(step, 10*step, step):
+                #  draw the vertical lines
+                qp.drawLine(i, 0, i, 5)
+
+                # We use font metrics to draw the text.
+                # We must know the width of the text in order to center it around the vertical line.
+                metrics = qp.fontMetrics()
+                fw = metrics.width(str(self.num[j]))
+                qp.drawText(i - fw/2, h/2, str(self.num[j]))
+
+                j = j + 1
+
+
+    class Example(QWidget):
+        def __init__(self):
+            super().__init__()
+
+            sld = QSlider(Qt.Horizontal, self)
+            sld.setFocusPolicy(Qt.NoFocus)
+            sld.setRange(1, OVER_CAPACITY)
+            sld.setValue(75)
+            sld.setGeometry(30, 40, 150, 30)
+
+            self.c = Communicate()
+            self.wid = BurningWidget()
+            self.c.updateBW[int].connect(self.wid.setValue)
+            sld.valueChanged[int].connect(self.changeValue)
+
+            hbox = QHBoxLayout()
+            hbox.addWidget(self.wid)
+
+            vbox = QVBoxLayout()
+            vbox.addStretch(1)
+            vbox.addLayout(hbox)
+            self.setLayout(vbox)
+
+            self.setGeometry(300, 300, 390, 210)
+            self.setWindowTitle("Burning Widget")
+            # self.show()
+
+        def changeValue(self, value):
+            self.c.updateBW.emit(value)
+            self.wid.repaint()
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
+
+
+def demo_file():
+    f = QFile("out/temp/playQT.spec")
+    ret = f.copy("out/temp/playQT.spec.new")
+    print("copy ret = ", ret)
+
+
 if __name__ == "__main__":
     print("case __main__")
     # demo_ui()buttonClicked
@@ -1046,4 +1161,6 @@ if __name__ == "__main__":
     # demo_widgets()
     # demo_widgets2()
     # demo_button_drag_drop()
-    demo_painter()
+    # demo_painter()
+    # demo_custom_widget()
+    demo_file()
