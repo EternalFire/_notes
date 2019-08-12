e061d593588c7b45b30c9cd99b40689a50aaa586
EternalFire <hzhfire@qq.com>
2019-08-12 07:33:46 +0700

    pyqt

---
+++ theme/python/playPYQT5/playQT_2.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+#
+#
+
+import sys, random, os
+from PyQt5 import QtWidgets, QtCore, QtGui
+from PyQt5.QtCore import (QDate, QTime, QDateTime, Qt, QTimeZone, pyqtSignal, QObject, QBasicTimer, QMimeData, \
+    QFile, QIODevice, QTextStream, QFileDevice, QUrl, QTextCodec, QDir)
+from PyQt5.QtGui import QIcon, QFont, QColor, QPixmap, QDrag, QPainter, QPen, QBrush, QPainterPath
+from PyQt5.QtWidgets import (QWidget, QToolTip,
+    QPushButton, QApplication, QMessageBox, QDesktopWidget,
+    QMainWindow, QMenu, QAction, QTextEdit, QLabel, qApp,
+    QHBoxLayout, QVBoxLayout, QGridLayout, QLineEdit,
+    QLCDNumber, QSlider, QInputDialog, QColorDialog, QFrame, QFileDialog, QFontDialog,
+    QCheckBox, QProgressBar, QCalendarWidget, QSplitter, QComboBox)
+
+
+def display_absolute_path():
+    class DisplayAbsolutePathWidget(QtWidgets.QWidget):
+        def __init__(self):
+            super().__init__()
+            self.initUI()
+
+        def initUI(self):
+            label = QLabel("drag file in: ", self)
+            line_edit = QLineEdit("", self)
+            self.line_edit = line_edit
+
+            layout = QGridLayout()
+            layout.addWidget(label, 0, 0)
+            layout.addWidget(line_edit, 0, 1, 0, 5)
+
+            self.setLayout(layout)
+            self.setAcceptDrops(True)
+            self.setMinimumWidth(567)
+            self.setMaximumHeight(self.sizeHint().height())
+
+        def dragEnterEvent(self, e: QtGui.QDragEnterEvent) -> None:
+            e.accept()
+
+        def dropEvent(self, e: QtGui.QDropEvent) -> None:
+            for url in e.mimeData().urls():
+                qurl = QUrl(url)
+                file_path = qurl.toLocalFile()
+                self.line_edit.setText(file_path)
+                self.line_edit.selectAll()
+                break
+
+    class SampleWindow(QMainWindow):
+        def __init__(self):
+            super().__init__()
+            widget = DisplayAbsolutePathWidget()
+            self.setCentralWidget(widget)
+
+
+    app = QApplication([])
+    widget = DisplayAbsolutePathWidget()
+    widget.show()
+    # window = SampleWindow()
+    # window.show()
+    sys.exit(app.exec_())
+
+    return
+
+
+if __name__ == "__main__":
+    print("playQt_2 !")
+    display_absolute_path()
+
