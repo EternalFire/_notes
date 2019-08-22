cea834f1528904b9d7f9975b35a331706c8713cc
EternalFire <hzhfire@qq.com>
2019-08-22 08:30:24 +0700

    pyqt

--- theme/python/playPYQT5/playQT_2.py
+++ theme/python/playPYQT5/playQT_2.py
@@ -2,10 +2,10 @@
 #
 #
 
-import sys, random, os
+import sys, random, os, json, time, signal
 from PyQt5 import QtWidgets, QtCore, QtGui, QtNetwork
-from PyQt5.QtCore import (QDate, QTime, QDateTime, Qt, QTimeZone, pyqtSignal, QObject, QBasicTimer, QMimeData, \
-    QFile, QIODevice, QTextStream, QFileDevice, QUrl, QTextCodec, QDir, QCoreApplication)
+from PyQt5.QtCore import (QDate, QTime, QDateTime, Qt, QTimeZone, pyqtSignal, QObject, QBasicTimer, QMimeData,
+    QFile, QIODevice, QTextStream, QFileDevice, QUrl, QTextCodec, QDir, QCoreApplication, QTimer, QThread)
 from PyQt5.QtGui import QIcon, QFont, QColor, QPixmap, QDrag, QPainter, QPen, QBrush, QPainterPath
 from PyQt5.QtWidgets import (QWidget, QToolTip,
     QPushButton, QApplication, QMessageBox, QDesktopWidget,
@@ -186,7 +186,7 @@ def networking_get_request():
 
         def doRequest(self):
 
-            url = "https://www.google.com"
+            url = "http://www.baidu.com"
             req = QtNetwork.QNetworkRequest(QUrl(url))
 
             self.nam = QtNetwork.QNetworkAccessManager()
@@ -213,10 +213,414 @@ def networking_get_request():
     sys.exit(app.exec_())
 
 
+def networking_post_request():
+    class Example:
+
+        def __init__(self):
+
+            self.doRequest()
+
+        def doRequest(self):
+
+            data = QtCore.QByteArray()
+            data.append("name=Peter&")
+            data.append("age=34")
+
+            url = "https://httpbin.org/post"
+            req = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
+            req.setHeader(QtNetwork.QNetworkRequest.ContentTypeHeader,
+                          "application/x-www-form-urlencoded")
+
+            self.nam = QtNetwork.QNetworkAccessManager()
+            self.nam.finished.connect(self.handleResponse)
+            self.nam.post(req, data)
+
+        def handleResponse(self, reply):
+
+            er = reply.error()
+
+            if er == QtNetwork.QNetworkReply.NoError:
+
+                bytes_string = reply.readAll()
+
+                json_ar = json.loads(str(bytes_string, 'utf-8'))
+                data = json_ar['form']
+
+                print('Name: {0}'.format(data['name']))
+                print('Age: {0}'.format(data['age']))
+
+                print()
+
+            else:
+                print("Error occurred: ", er)
+                print(reply.errorString())
+
+            QtCore.QCoreApplication.quit()
+
+    app = QtCore.QCoreApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+def networking_head_request():
+    class Example:
+
+        def __init__(self):
+
+            self.doRequest()
+
+        def doRequest(self):
+
+            url = "http://www.baidu.com"
+            req = QtNetwork.QNetworkRequest(QUrl(url))
+
+            self.nam = QtNetwork.QNetworkAccessManager()
+            self.nam.finished.connect(self.handleResponse)
+            self.nam.head(req)
+
+        def handleResponse(self, reply):
+
+            er = reply.error()
+
+            if er == QtNetwork.QNetworkReply.NoError:
+
+                for k, v in reply.rawHeaderPairs():
+                    print(str(k, 'UTF-8'), ": ", str(v, 'UTF-8'))
+
+            else:
+                print("Error occurred: ", er)
+                print(reply.errorString())
+
+            QCoreApplication.quit()
+
+    app = QCoreApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+def networking_authenticate():
+    class Example:
+
+        def __init__(self):
+
+            self.doRequest()
+
+        def doRequest(self):
+
+            self.auth = 0
+
+            url = "https://httpbin.org/basic-auth/user7/passwd7"
+            req = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
+
+            self.nam = QtNetwork.QNetworkAccessManager()
+            self.nam.authenticationRequired.connect(self.authenticate)
+            self.nam.finished.connect(self.handleResponse)
+            self.nam.get(req)
+
+        def authenticate(self, reply, auth):
+
+            print("Authenticating")
+
+            self.auth += 1
+
+            if self.auth >= 3:
+                reply.abort()
+
+            auth.setUser("user7")
+            auth.setPassword("passwd7")
+
+        def handleResponse(self, reply):
+
+            er = reply.error()
+
+            if er == QtNetwork.QNetworkReply.NoError:
+
+                bytes_string = reply.readAll()
+
+                data = json.loads(str(bytes_string, 'utf-8'))
+
+                print('Authenticated: {0}'.format(data['authenticated']))
+                print('User: {0}'.format(data['user']))
+
+                print()
+
+            else:
+                print("Error occurred: ", er)
+                print(reply.errorString())
+
+            QtCore.QCoreApplication.quit()
+
+    app = QtCore.QCoreApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+def networking_fetch_favicon():
+    class Example:
+
+        def __init__(self):
+
+            self.doRequest()
+
+        def doRequest(self):
+
+            # url = "http://www.google.com/favicon.ico"
+            url = "https://github.com/fluidicon.png"
+            req = QtNetwork.QNetworkRequest(QtCore.QUrl(url))
+
+            self.nam = QtNetwork.QNetworkAccessManager()
+            self.nam.finished.connect(self.handleResponse)
+            self.nam.get(req)
+
+        def handleResponse(self, reply):
+
+            er = reply.error()
+
+            if er == QtNetwork.QNetworkReply.NoError:
+
+                data = reply.readAll()
+                self.saveFile(data)
+
+            else:
+                print("Error occured: ", er)
+                print(reply.errorString())
+
+            QtCore.QCoreApplication.quit()
+
+        def saveFile(self, data):
+
+            f = open('favicon.ico', 'wb')
+
+            with f:
+                f.write(data)
+
+    app = QtCore.QCoreApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+def networking_block():
+    class Example(QObject):
+
+        def __init__(self):
+            super().__init__()
+            print(QDateTime.currentDateTime().toString())
+            QTimer.singleShot(3000, self.openConnection)
+            QTimer.singleShot(4000, self.doSomeWork)
+
+        def openConnection(self):
+            print(QDateTime.currentDateTime().toString())
+            tcpSocket = QtNetwork.QTcpSocket()
+            tcpSocket.connectToHost("www.baidu.com", 80)
+
+            tcpSocket.write(b"GET / HTTP/1.1\r\n" \
+                            b"Host: www.baidu.com\r\n" \
+                            b"\r\n")
+
+            while tcpSocket.waitForReadyRead():
+                bytes_string = tcpSocket.readAll()
+                print(str(bytes_string, 'utf-8'))
+
+            if tcpSocket.error():
+                print(tcpSocket.errorString())
+
+            tcpSocket.close()
+
+        def doSomeWork(self):
+
+            print(QDateTime.currentDateTime().toString(), "Doing some work")
+            QCoreApplication.quit()
+
+    app = QCoreApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+def networking_nonblock():
+    class Worker(QThread):
+
+        def __init__(self):
+            super(Worker, self).__init__()
+
+            print("Init Thread")
+
+        def run(self):
+            print("Thread started")
+
+            time.sleep(10)
+
+            print("Thread ended")
+
+    class Example(QObject):
+
+        def __init__(self):
+            super().__init__()
+
+            self.openConnection()
+            self.doSomeWork()
+
+        def openConnection(self):
+
+            self.tcpSocket = QtNetwork.QTcpSocket()
+            self.tcpSocket.readyRead.connect(self.readData)
+            self.tcpSocket.error.connect(self.onError)
+
+            self.tcpSocket.connectToHost("pypi.org", 80)
+
+            self.tcpSocket.write(b"GET / HTTP/1.1\r\n" \
+                                 b"Host: pypi.org\r\n" \
+                                 b"\r\n")
+
+        def doSomeWork(self):
+
+            self.wrk = Worker()
+            self.wrk.finished.connect(QCoreApplication.quit)
+            self.wrk.start()
+
+        def onError(self, e):
+
+            t = QtNetwork.QAbstractSocket.RemoteHostClosedError
+
+            if e == t:
+                print("closing connection")
+
+            else:
+                print("error")
+                print(self.tcpSocket.errorString())
+
+        def readData(self):
+
+            while not self.tcpSocket.atEnd():
+                bytes_string = self.tcpSocket.readAll()
+                print(str(bytes_string, 'utf-8'))
+
+            self.tcpSocket.close()
+
+    app = QCoreApplication([])
+    ex = Example()
+    sys.exit(app.exec_())
+
+
+def networking_echo_server():
+    class EchoSocket(QtNetwork.QTcpSocket):
+
+        def __init__(self, p):
+            super().__init__()
+
+            self.readyRead.connect(self.readClient)
+            self.disconnected.connect(self.discardClient)
+
+            self.error.connect(self.onError)
+
+        def readClient(self):
+
+            msg = self.readLine()
+            print(msg)
+            self.write(msg)
+
+        def discardClient(self):
+
+            self.deleteLater()
+
+        def onError(self, e):
+
+            t = QtNetwork.QAbstractSocket.RemoteHostClosedError
+
+            if e == t:
+                print("closing connection")
+
+            else:
+                print("error")
+                print(self.tcp.errorString())
+
+    class EchoServer(QtNetwork.QTcpServer):
+
+        def __init__(self, port):
+            super().__init__()
+
+            print("Starting Echo Server")
+            print("Listening on port:", port)
+            self.listen(QtNetwork.QHostAddress.LocalHost, port)
+
+        def incomingConnection(self, socket):
+            self.es = EchoSocket(self)
+            self.es.setSocketDescriptor(socket)
+
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+    app = QCoreApplication([])
+    echo = EchoServer(6001)
+    sys.exit(app.exec_())
+
+
+def networking_echo_client():
+    class EchoClient(QObject):
+
+        def __init__(self):
+            super().__init__()
+
+            self.getMessage()
+            self.openConnection()
+
+        def getMessage(self):
+
+            args = sys.argv
+
+            if len(args) == 2:
+
+                self.msg = bytearray(args[1], 'UTF-8')
+
+            else:
+                print("Usage: echo_client.py message")
+                sys.exit(1)
+
+        def openConnection(self):
+
+            self.tcpSocket = QtNetwork.QTcpSocket()
+            self.tcpSocket.readyRead.connect(self.readData)
+            self.tcpSocket.error.connect(self.onError)
+
+            self.tcpSocket.connectToHost("localhost", 6001)
+
+            self.tcpSocket.write(self.msg)
+
+        def onError(self, e):
+
+            t = QtNetwork.QAbstractSocket.RemoteHostClosedError
+
+            if e == t:
+                print("closing connection")
+
+            else:
+                print("error")
+                print(self.tcp.errorString())
+
+        def readData(self):
+
+            while not self.tcpSocket.atEnd():
+                bytes_string = self.tcpSocket.readAll()
+                print(str(bytes_string, 'utf-8'))
+
+            self.tcpSocket.close()
+            QCoreApplication.quit()
+
+    app = QCoreApplication([])
+    ec = EchoClient()
+    sys.exit(app.exec_())
+
+
 if __name__ == "__main__":
     print("playQt_2 !")
     # display_absolute_path()
     # networking_network_interface()
     # networking_get_hostname()
     # networking_get_ip()
-    networking_get_request()
+    # networking_get_request()
+    # networking_post_request()
+    # networking_head_request()
+    # networking_authenticate()
+    # networking_fetch_favicon()
+    # networking_block()
+    # networking_nonblock()
+    # networking_echo_server()
+    networking_echo_client()
